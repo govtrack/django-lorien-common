494fe3f4a0ba71e88a2bf920cc2857985644dbc1
Gregory Petukhov <lorien@lorien.name>
2010-01-06 23:09:33 +0600

    Add JSONField to fields module

--- common/fields.py
+++ common/fields.py
@@ -1,10 +1,7 @@
-"""
-Details about AutoOneToOneField:
-    http://softwaremaniacs.org/blog/2007/03/07/auto-one-to-one-field/
-"""
-
-from django.db.models import OneToOneField
+from django.db import models
 from django.db.models.fields.related import SingleRelatedObjectDescriptor 
+from django.core.serializers.json import DjangoJSONEncoder
+from django.utils import simplejson
 
 
 class AutoSingleRelatedObjectDescriptor(SingleRelatedObjectDescriptor):
@@ -17,11 +14,42 @@ class AutoSingleRelatedObjectDescriptor(SingleRelatedObjectDescriptor):
             return obj
 
 
-class AutoOneToOneField(OneToOneField):
+class AutoOneToOneField(models.OneToOneField):
     """
     OneToOneField creates dependent object on first request from parent object
     if dependent oject has not created yet.
+
+    Origin: http://softwaremaniacs.org/blog/2007/03/07/auto-one-to-one-field/
     """
 
     def contribute_to_related_class(self, cls, related):
         setattr(cls, related.get_accessor_name(), AutoSingleRelatedObjectDescriptor(related))
+
+
+class JSONField(models.TextField):
+    """
+    JSONField is a generic textfield that neatly serializes/unserializes
+    JSON objects seamlessly.
+
+    Origin: http://www.djangosnippets.org/snippets/1478/
+    """
+
+    __metaclass__ = models.SubfieldBase
+
+    def to_python(self, value):
+        if value == "":
+            return None
+
+        try:
+            if isinstance(value, basestring):
+                return simplejson.loads(value)
+        except ValueError:
+            pass
+        return value
+
+    def get_db_prep_save(self, value):
+        if value == "":
+            return None
+        if isinstance(value, dict):
+            value = simplejson.dumps(value, cls=DjangoJSONEncoder)
+        return super(JSONField, self).get_db_prep_save(value)
