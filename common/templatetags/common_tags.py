1999214758fc806c1b6d53c98aa79e21585e361f
Gregory Petukhov <lorien@lorien.name>
2010-03-10 15:29:20 +0600

    Fix alter_qs template tag

--- common/templatetags/common_tags.py
+++ common/templatetags/common_tags.py
@@ -15,13 +15,19 @@ def alter_qs(qs, name, value, name2=None, value2=None):
     """
 
     qs = qs.lstrip('?')
-    args = dict((x[0], smart_str(x[1])) for x in parse_qsl(qs))
+    args = [[x[0], smart_str(x[1])] for x in parse_qsl(qs)]
     if value:
-        args[name] = smart_str(value)
+        found = False
+        for arg in args:
+            if arg[0] == name:
+                arg[1] = smart_str(value)
+                found = True
+        if not found:
+            args.append([name, smart_str(value)])
     else:
-        if name in args:
-            del args[name]
+        args = [x for x in args if x[0] != name]
     if args:
+        args = [tuple(x) for x in args]
         result = '?' + urlencode(args)
         if name2 and value2:
             return alter_qs(result, name2, value2)
