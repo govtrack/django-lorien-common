134db28f297d3b3a191f01fe65e94237915c55f1
Gregory Petukhov <lorien@lorien.name>
2010-04-18 10:32:34 +0700

    Use simplejson from django

--- common/http.py
+++ common/http.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-import simplejson
-
+from django.utils import simplejson
 from django.template.loader import render_to_string
 from django.http import HttpResponseRedirect, HttpResponse
 from django.conf import settings
