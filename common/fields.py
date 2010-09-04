d6bde6db938abb42fb1df4f55b28c3a909498d33
Gregory Petukhov <lorien@lorien.name>
2010-09-04 21:43:43 +0700

    Change JSONField implementation

--- common/fields.py
+++ common/fields.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.fields.related import SingleRelatedObjectDescriptor 
 from django.core.serializers.json import DjangoJSONEncoder
+from django.conf import settings
+from django.forms.widgets import Textarea
 from django.utils import simplejson
 
 
@@ -26,30 +28,46 @@ class AutoOneToOneField(models.OneToOneField):
         setattr(cls, related.get_accessor_name(), AutoSingleRelatedObjectDescriptor(related))
 
 
+
+
 class JSONField(models.TextField):
-    """
-    JSONField is a generic textfield that neatly serializes/unserializes
-    JSON objects seamlessly.
-
-    Origin: http://www.djangosnippets.org/snippets/1478/
-    """
-
     __metaclass__ = models.SubfieldBase
 
+    def contribute_to_class(self, cls, name):
+        super(JSONField, self).contribute_to_class(cls, name)
+
+        def get_json(model):
+            return self.get_db_prep_value(getattr(model, self.attname))
+        setattr(cls, 'get_%s_json' % self.name, get_json)
+
+        def set_json(model, json):
+            setattr(model, self.attname, self.to_python(json))
+        setattr(cls, 'set_%s_json' % self.name, set_json)
+
+    def formfield(self, **kwargs):
+        kwargs['widget'] = JSONWidget(attrs={'class': 'vLargeTextField'})
+        return super(JSONField, self).formfield(**kwargs)
+
+    def get_db_prep_value(self, value):
+        return simplejson.dumps(value)
+
     def to_python(self, value):
-        if value == "":
-            return None
+        if not isinstance(value, basestring):
+            return value
 
         try:
-            if isinstance(value, basestring):
-                return simplejson.loads(value)
-        except ValueError:
-            pass
-        return value
+            return simplejson.loads(value, encoding=settings.DEFAULT_CHARSET)
+        except ValueError, e:
+            # If string could not parse as JSON it's means that it's Python
+            # string saved to JSONField.
+            return value
 
-    def get_db_prep_save(self, value):
-        if value == "":
-            return None
-        if isinstance(value, dict):
-            value = simplejson.dumps(value, cls=DjangoJSONEncoder)
-        return super(JSONField, self).get_db_prep_save(value)
+
+class JSONWidget(Textarea):
+    """
+    Prettify dumps of all non-string JSON data.
+    """
+    def render(self, name, value, attrs=None):
+        if not isinstance(value, basestring) and value is not None:
+            value = simplejson.dumps(value, indent=4, sort_keys=True)
+        return super(JSONWidget, self).render(name, value, attrs)
