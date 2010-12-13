7a6c9c95aa2d5ca8ad25310e31e04714e3f41409
Gregory Petukhov <lorien@lorien.name>
2010-12-14 03:40:11 +0600

    Add image module which generates random image and wrap it into django.core.files.File instance

--- common/data/app_template/views.py
+++ common/data/app_template/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 
 from common.decorators import render_to
+from common.pagination import paginate
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 PACKAGE = 'common'
-VERSION = '0.1.28'
+VERSION = '0.1.29'
 
 if __name__ == '__main__':
     # Compile the list of packages available, because distutils doesn't have
