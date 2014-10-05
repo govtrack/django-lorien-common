274ac0c84088c85dabce803643bc0c2596ee84bc
Joshua Tauberer <jt@occams.info>
2014-10-05 18:24:42 -0400

    Django 1.7

--- common/http.py
+++ common/http.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.utils import simplejson
+import json
 from django.template.loader import render_to_string
 from django.http import HttpResponseRedirect, HttpResponse
 from django.conf import settings
 
 class HttpResponseJson(HttpResponse):
     def __init__(self, data):
-        json_data = simplejson.dumps(data)
+        json_data = json.dumps(data)
         super(HttpResponseJson, self).__init__(
             content=json_data, mimetype='application/json')
 
